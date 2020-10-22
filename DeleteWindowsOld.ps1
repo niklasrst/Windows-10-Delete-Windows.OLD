5232f4185d1c06cf76a01eab7b434217e56d140f
niklasrst <47035125+niklasrst@users.noreply.github.com>
2020-10-22 10:46:33 +0200

    Update DeleteWindowsOld.ps1
    Added Transcripts

--- DeleteWindowsOld.ps1
+++ DeleteWindowsOld.ps1
@@ -1,17 +1,20 @@
-﻿<#
-The sample scripts are not supported under any Microsoft standard support 
-program or service. The sample scripts are provided AS IS without warranty  
-of any kind. Microsoft further disclaims all implied warranties including,  
-without limitation, any implied warranties of merchantability or of fitness for 
-a particular purpose. The entire risk arising out of the use or performance of  
-the sample scripts and documentation remains with you. In no event shall 
-Microsoft, its authors, or anyone else involved in the creation, production, or 
-delivery of the scripts be liable for any damages whatsoever (including, 
-without limitation, damages for loss of business profits, business interruption, 
-loss of business information, or other pecuniary loss) arising out of the use 
-of or inability to use the sample scripts or documentation, even if Microsoft 
-has been advised of the possibility of such damages.
-#> 
+<#
+    .SYNOPSIS 
+    Windows 10 Remove C:\Windows.old
+
+    .DESCRIPTION
+    Install:   PowerShell.exe -ExecutionPolicy Bypass -Command .\W10_RemoveWindowsOLD.ps1
+
+    .ENVIRONMENT
+    PowerShell 5.0
+
+    .AUTHOR
+    Niklas Rast
+#>
+
+$logFile = ('{0}\{1}.log' -f $env:Temp, [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name))
+Start-Transcript -path $logFile
+
 #Check if the script run with administrator privilege
 If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {   
@@ -35,5 +38,4 @@ Else
 	Write-Warning "There is no 'Windows.old' folder in system driver"
     cmd /c pause 
 }
-		
-
+Stop-Transcript
